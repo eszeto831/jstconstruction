16bb27602a9b66acaafcda03592df150475f6ed8
Edmond Szeto <eszeto831@gmail.com>
2016-07-30 19:15:05 -0700

    services initial commit

--- content.js
+++ content.js
@@ -1,4 +1,29 @@
 $( document ).ready(function() {
   console.log("ehehe")
     $('#aboutSub').html(homeData.text);
+
+	var servicesHtml = "";
+	for(serviceData in servicesData.services)
+	{
+		
+	    var serviceString = "<div class=&quot;col-md-4&quot;>hllo</div>";
+
+		/*
+	    var serviceString = "
+            <div class=&quot;col-md-4&quot;>
+                <h4 class=&quot;service-heading&quot;>E-Commerce</h4>
+                <p class=&quot;text-muted&quot;>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+";
+*/
+//var serviceString = "test";
+
+		console.log(serviceString);
+		servicesHtml = servicesHtml.concat(serviceString);
+		console.log(servicesHtml);
+	}
+
+	console.log(servicesHtml);
+
+    $("#servicesDiv").html(servicesHtml);
 });
