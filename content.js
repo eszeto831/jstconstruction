e8d0523c9da5d2cd4b758c3e53e7d1a83fc76b03
Edmond Szeto <eszeto831@gmail.com>
2016-08-07 14:40:42 -0700

    clean up

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
-  console.log("ehehe")
     $('#aboutSub').html(homeData.text);
 
 	var servicesHtml = "";
-	for(serviceData in servicesData.services)
+	for(index in servicesData.services)
 	{
+		var serviceInfo = servicesData.services[index];
 	    var serviceString = ""+
             "<div class=\"col-md-4\">"+
                 "<span class=\"fa-stack fa-4x\">"+
@@ -12,41 +12,10 @@ $( document ).ready(function() {
                     "<i class=\"fa fa-shopping-cart fa-stack-1x fa-inverse\"></i>"+
                 "</span>"+
                 "<h4 class=\"service-heading\">E-Commerce</h4>"+
-                "<p class=\"text-muted\">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>"+
+                "<p class=\"text-muted\">"+serviceInfo.name+"</p>"+
             "</div>";
-		
-	    //var serviceString = "<div class=&quot;col-md-4&quot;>hllo</div>";
-/*
-	    var serviceString = ""+
-            "<div class=&quot;col-md-4&quot;>"+
-                "<span class=&quot;fa-stack fa-4x&quot;>"+
-                    "<i class=&quot;fa fa-circle fa-stack-2x text-primary&quot;></i>"+
-                    "<i class=&quot;fa fa-shopping-cart fa-stack-1x fa-inverse&quot;></i>"+
-                "</span>"+
-                "<h4 class=&quot;service-heading&quot;>E-Commerce</h4>"+
-                "<p class=&quot;text-muted&quot;>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>"+
-            "</div>";
-*/
-		/*
-	    var serviceString = "
-            <div class=&quot;col-md-4&quot;>
-                <span class=&quot;fa-stack fa-4x&quot;>
-                    <i class=&quot;fa fa-circle fa-stack-2x text-primary&quot;></i>
-                    <i class=&quot;fa fa-shopping-cart fa-stack-1x fa-inverse&quot;></i>
-                </span>
-                <h4 class=&quot;service-heading&quot;>E-Commerce</h4>
-                <p class=&quot;text-muted&quot;>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-            </div>
-";
-*/
-//var serviceString = "test";
-
-		console.log(serviceString);
 		servicesHtml = servicesHtml.concat(serviceString);
-		console.log(servicesHtml);
 	}
-
-	console.log(servicesHtml);
 
     $("#servicesDiv").html(servicesHtml);
 });
