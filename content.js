ec2b577f0d2f5120532aee8e21af880cb474711e
Edmond Szeto <eszeto831@gmail.com>
2016-08-14 17:38:40 -0700

    fixing css issues on small devices

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 	{
 		var serviceInfo = servicesData.services[index];
 	    var serviceString = ""+
-                "<div class=\"col-md-3 col-xs-3\">"+
+                "<div class=\"col-md-4\">"+
                     "<span class=\"fa-stack fa-4x\">"+
                         "<i class=\"fa fa-circle fa-stack-2x text-primary\"></i>"+
                         //"<i class=\"fa fa-shopping-cart fa-stack-1x fa-inverse\"></i>"+
