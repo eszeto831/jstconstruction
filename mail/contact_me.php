4d9d33b280913f4d7f98b8e3f4e2d6e2fca39229
eszeto-perfectstorm <eszeto831@gmail.com>
2025-05-24 14:08:40 -1000

    switch over to pwong

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,7 +15,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'peter@jstconstruction.net'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'pwong0124@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@jstcontruction.net\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
@@ -24,7 +24,7 @@ mail($to,$email_subject,$email_body,$headers);
 
 //auto reply
 $to = "$email_address"; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "JST Construction";
+$email_subject = "PW Construction Group";
 $email_body = "Thank you for contacting us. We will respond as soon as possible.";
 $headers = "From: noreply@jstcontruction.net\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: noreply@jstcontruction.net";   
