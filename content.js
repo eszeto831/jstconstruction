4223aad43aa673dc701c67a2f4a0182a9a84abc0
eszeto-perfectstorm <eszeto831@gmail.com>
2026-03-22 17:22:54 -1000

    add new tabs

--- content.js
+++ content.js
@@ -27,6 +27,75 @@ $( document ).ready(function() {
 
     $("#servicesDiv").html(servicesHtml);
 
+	var energyHtml = "";
+	for(index in energyData.services)
+	{
+		var energyInfo = energyData.services[index];
+	    var energyString = ""+
+                "<div class=\"col-md-4 col-sm-6\">"+
+                    "<span class=\"fa-stack fa-4x\">"+
+                        "<i class=\"fa fa-circle fa-stack-2x text-primary\"></i>"+
+                        //"<i class=\"fa fa-shopping-cart fa-stack-1x fa-inverse\"></i>"+
+                    	"<img src=\""+energyInfo.img+"\" class=\"fa fa-circle fa-stack-1x img-circle\" alt=\"\">"+
+                    "</span>"+
+                    //"<img src=\""+serviceInfo.img+"\" class=\"img-responsive img-circle\" alt=\"\">"+
+                	"<div class=\"portfolio-caption\">"+
+                        "<h4>"+energyInfo.name+"</h4>"+
+                        "<h3 class=\"section-subheading text-muted\">"+energyInfo.description+"</h3>"+
+                    "</div>"+
+                "</div>";
+		energyHtml = energyHtml.concat(energyString);
+	}
+    //"<i class=\"fa fa-shopping-cart fa-stack-1x fa-inverse\"></i>"
+
+    $("#energyDiv").html(energyHtml);
+
+	var automationHtml = "";
+	for(index in automationData.services)
+	{
+		var automationInfo = automationData.services[index];
+	    var automationString = ""+
+                "<div class=\"col-md-4 col-sm-6\">"+
+                    "<span class=\"fa-stack fa-4x\">"+
+                        "<i class=\"fa fa-circle fa-stack-2x text-primary\"></i>"+
+                        //"<i class=\"fa fa-shopping-cart fa-stack-1x fa-inverse\"></i>"+
+                    	"<img src=\""+automationInfo.img+"\" class=\"fa fa-circle fa-stack-1x img-circle\" alt=\"\">"+
+                    "</span>"+
+                    //"<img src=\""+serviceInfo.img+"\" class=\"img-responsive img-circle\" alt=\"\">"+
+                	"<div class=\"portfolio-caption\">"+
+                        "<h4>"+automationInfo.name+"</h4>"+
+                        "<h3 class=\"section-subheading text-muted\">"+automationInfo.description+"</h3>"+
+                    "</div>"+
+                "</div>";
+		automationHtml = automationHtml.concat(automationString);
+	}
+    //"<i class=\"fa fa-shopping-cart fa-stack-1x fa-inverse\"></i>"
+
+    $("#automationDiv").html(automationHtml);
+
+	var lightingHtml = "";
+	for(index in lightingData.services)
+	{
+		var lightingInfo = lightingData.services[index];
+	    var lightingString = ""+
+                "<div class=\"col-md-4 col-sm-6\">"+
+                    "<span class=\"fa-stack fa-4x\">"+
+                        "<i class=\"fa fa-circle fa-stack-2x text-primary\"></i>"+
+                        //"<i class=\"fa fa-shopping-cart fa-stack-1x fa-inverse\"></i>"+
+                    	"<img src=\""+lightingInfo.img+"\" class=\"fa fa-circle fa-stack-1x img-circle\" alt=\"\">"+
+                    "</span>"+
+                    //"<img src=\""+serviceInfo.img+"\" class=\"img-responsive img-circle\" alt=\"\">"+
+                	"<div class=\"portfolio-caption\">"+
+                        "<h4>"+lightingInfo.name+"</h4>"+
+                        "<h3 class=\"section-subheading text-muted\">"+lightingInfo.description+"</h3>"+
+                    "</div>"+
+                "</div>";
+		lightingHtml = lightingHtml.concat(lightingString);
+	}
+    //"<i class=\"fa fa-shopping-cart fa-stack-1x fa-inverse\"></i>"
+
+    $("#lightingDiv").html(lightingHtml);
+
 	var portfolioHtml = "";
 	for(index in projectData.projects)
 	{
